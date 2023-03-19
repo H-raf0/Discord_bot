74a5ceef2ff1c47df92c34146925760f812d3c65
Achraf El Allali <Achraf.EL_ALLALI@etu.uca.fr>
2023-03-19 23:44:31 +0100

    wasted my time trying to fix something i already fixed and maybe changed something else

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def login():
 
 def checkMsg():
     try:
-        driver.find_element(by=By.CSS_SELECTOR,
-                            value='#app-mount > div.appAsidePanelWrapper-ev4hlp > div.notAppAsidePanel-3yzkgB > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div > nav > ul > div.scroller-3X7KbA.none-1rXy4P.scrollerBase-1Pkza4 > div:nth-child(2)')
+        driver.find_element(by=By.XPATH,
+                            value='//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/nav/ul/div[2]/div[2]/div/div[2]/div')
     except:
         print("no msg or error")
         return False
@@ -35,18 +35,29 @@ def checkMsg():
         return True
 
 def readMsgs():
-    liMsift = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, '#app-mount > div.appAsidePanelWrapper-ev4hlp > div.notAppAsidePanel-3yzkgB > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div > nav > ul > div.scroller-3X7KbA.none-1rXy4P.scrollerBase-1Pkza4 > div:nth-child(2)'))
-    )
+    found = False
+    while not found:
+        try:
+            liMsift = driver.find_element(by=By.XPATH, value='//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/nav/ul/div[2]/div[2]/div/div[2]/div')
+        except:
+            print("can't find other user icon")
+        else:
+            found = True
 
     liMsift.click()
-    msg = WebDriverWait(driver, 10).until(
+
+    msgs = WebDriverWait(driver, 2).until(
         EC.visibility_of_all_elements_located((By.XPATH, "//*[starts-with(@id, 'message-content-')]"))
     )
     msgLst = []
-    for e in msg:
+    for e in msgs:
         msgLst.append(e.text)
-        # print(e.text)
+        print(e)
+
+    #users = driver.find_elements(by=By.XPATH, value="//*[starts-with(@id, 'chat-messages-')]/div/div[1]/h3/span/span")
+    #userName = users[-1].text
+
+
     return msgLst
 
 def doAction(action):
@@ -59,6 +70,8 @@ def doAction(action):
 
     writingBar = driver.find_element(by=By.XPATH, value='//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/div/div/div[2]/div[2]/main/form/div/div[1]/div/div[3]/div/div[2]/div')
     writingBar.send_keys(msg + Keys.ENTER)
+    # quit conversation
+    driver.find_element(by=By.XPATH, value='//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/div/div/div/nav/div[2]/ul/li[1]/div/a/div/div[2]').click()
 
 
 if __name__ == "__main__":
@@ -77,13 +90,11 @@ if __name__ == "__main__":
 
     print(driver.title)
 
-    while not checkMsg():
-        sleep(1)
+    while 1:
+        while not checkMsg():
+            sleep(1)
+        msglst = readMsgs()
+        doAction(msglst[-1])
 
 
-    msglst = readMsgs()
-
-    doAction(msglst[-1])
-
-
-    driver.quit()
+    # driver.quit()
